8fbe211cffaed30d313012fdd4651f5c493f2bb5
Brais Barboza Ordóñez <brais.barboza@udc.es>
2022-02-16 16:44:53 +0100

    Clase 2

--- ejercicio.c
+++ ejercicio.c
@@ -1,53 +1,161 @@
+#include <stdio.h>
+#include <math.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 
-#define WIDTH 500
-#define HEIGTH 500
-#define ORIGIN_X 50
-#define ORIGIN_Y 50
+GLint ancho=400;
+GLint alto=400;
+
+GLfloat anguloCuboX = 0.0f;
+GLfloat anguloCuboY = 0.0f;
+GLfloat anguloEsfera = 0.0f;
+
+int hazPerspectiva = 0;
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    if(hazPerspectiva)
+        gluPerspective(60.0f, (GLfloat)width/(GLfloat)height, 1.0f, 20.0f);
+    else       
+        glOrtho(-4,4, -4, 4, 1, 10);
+
+    glMatrixMode(GL_MODELVIEW);
+ 
+    ancho = width;
+    alto = height;
+}
+
+void drawCube(void)
+{
+    glColor3f(1.0f, 0.0f, 0.0f); // Rojo
+    glBegin(GL_QUADS);       //cara frontal
+    glVertex3f(-1.0f, -1.0f,  1.0f); 
+    glVertex3f( 1.0f, -1.0f,  1.0f); 
+    glVertex3f( 1.0f,  1.0f,  1.0f); 
+    glVertex3f(-1.0f,  1.0f,  1.0f); 
+    glEnd();
+
+    glColor3f(0.0f, 1.0f, 0.0f); // Verde
+    glBegin(GL_QUADS);       //cara trasera
+    glVertex3f( 1.0f, -1.0f, -1.0f); 
+    glVertex3f(-1.0f, -1.0f, -1.0f); 
+    glVertex3f(-1.0f,  1.0f, -1.0f); 
+    glVertex3f( 1.0f,  1.0f, -1.0f); 
+    glEnd();
+
+    glColor3f(0.0f, 0.0f, 1.0f); // Azul
+    glBegin(GL_QUADS);       //cara lateral izq
+    glVertex3f(-1.0f,-1.0f, -1.0f); 
+    glVertex3f(-1.0f,-1.0f,  1.0f); 
+    glVertex3f(-1.0f, 1.0f,  1.0f); 
+    glVertex3f(-1.0f, 1.0f, -1.0f); 
+    glEnd();
+
+    glColor3f(1.0f, 1.0f, 0.0f); // Amarillo
+    glBegin(GL_QUADS);       //cara lateral dcha
+    glVertex3f(1.0f, -1.0f,  1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f,  1.0f, -1.0f);
+    glVertex3f(1.0f,  1.0f,  1.0f);
+    glEnd(); 
+    
+    glColor3f(0.0f, 1.0f, 1.0f); // Cyan
+    glBegin(GL_QUADS);       //cara arriba
+    glVertex3f(-1.0f, 1.0f,  1.0f);
+    glVertex3f( 1.0f, 1.0f,  1.0f);
+    glVertex3f( 1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glEnd();
+ 
+    glColor3f(1.0f, 0.0f, 1.0f); // Magenta
+    glBegin(GL_QUADS);       //cara abajo
+    glVertex3f( 1.0f,-1.0f, -1.0f);
+    glVertex3f( 1.0f,-1.0f,  1.0f);
+    glVertex3f(-1.0f,-1.0f,  1.0f);
+    glVertex3f(-1.0f,-1.0f, -1.0f);
+    glEnd();
+}
+
+void display()
+{ 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    glTranslatef(0.0f, 0.0f, -5.0f);
+ 
+    glRotatef(anguloCuboX, 1.0f, 0.0f, 0.0f);
+    glRotatef(anguloCuboY, 0.0f, 1.0f, 0.0f); 
+    drawCube();
+ 
+    glLoadIdentity();
+ 
+    glTranslatef(0.0f, 0.0f, -5.0f);
+    glRotatef(anguloEsfera, 0.0f, 1.0f, 0.0f);
+    glTranslatef(3.0f, 0.0f, 0.0f);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glutWireSphere(0.5f, 8, 8);
+ 
+    glFlush();
+    glutSwapBuffers();
+ 
+    anguloCuboX+=0.1f;
+    anguloCuboY+=0.1f;
+    anguloEsfera+=0.2f;
+}
 
 void init()
 {
-    glClearColor(0,0,0,0);/*Fijar el color por defecto a negro en el formato (RGBA)*/
+    glClearColor(0,0,0,0);
+    glEnable(GL_DEPTH_TEST);
+    ancho = 400;
+    alto = 400;
 }
+ 
 
-void trianguloBlanco(){
-    glColor3f( 1.0f, 1.0f, 1.0f);
-    glBegin(GL_TRIANGLES);
-    glVertex3f(-1.0f, -1.0f, 0.0f); //V1
-    glVertex3f(1.0f, -1.0f, 0.0f); //V2
-    glVertex3f(0.0f, 1.0f, 0.0f); //V3
-    glEnd();
-}
-void display()
+void idle()
 {
-    glClear(GL_COLOR_BUFFER_BIT); /*Borrado del FrameBuffer*/ 
-    glMatrixMode(GL_MODELVIEW); /* Activar las modificaciones en los objetos del mundo*/
-    glLoadIdentity();/* Cargar la identidad en la matrix de modeficaciones del mundo*/
-    trianguloBlanco();
-    /* Lo que haya que pintar ira aquí*/
-
-    glutSwapBuffers(); /*Forzar el renderizado*/
+    display();
 }
-
-
-void reshape(int w, int h)
+ 
+void keyboard(unsigned char key, int x, int y)
 {
-    glViewport(0, 0, 500, 500);
-    glMatrixMode(GL_PROJECTION);/*Activar las modificaciones en la camara*/
-    glLoadIdentity(); /*Cargar la identidad en la matrix de proyección*/
-    glOrtho(-1, 1, -1, 1, -1, 1);
+    switch(key)
+    {
+    case 'p':
+    case 'P':
+      hazPerspectiva=1;
+      reshape(ancho,alto);
+      break;
+ 
+    case 'o':
+    case 'O':
+      hazPerspectiva=0;
+      reshape(ancho,alto);
+      break;
+ 
+    case 27:   // escape
+      exit(0);
+      break;
+    }
 }
-
-int main(int argc, char** argv)
+ 
+int main(int argc, char **argv)
 {
-    glutInit(&argc, argv); /*Cargar el teclado y el ráton*/
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); /*inicializar el Frame Buffer GLUT_DOUBLE O GLUT_SIMPLE para uno o 2 buffers*/
-    glutInitWindowSize(WIDTH, HEIGTH); /*Tamaño de la ventana*/ 
-    glutInitWindowPosition(ORIGIN_X, ORIGIN_Y); /*Posición del extremo superior-izquierdo de la ventana*/
-    glutCreateWindow("Hola Mundo"); /* Crear la ventana*/
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB| GLUT_DEPTH);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(ancho, alto);
+    glutCreateWindow("Cubo");
     init();
-    glutDisplayFunc(display); /*Activar la funcion de pintado*/
-    glutReshapeFunc(reshape); /*Activar la funcion de escalado*/
-    glutMainLoop(); /*Arrancar el bucle de OpenGL*/
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(idle);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
     return 0;
 }
